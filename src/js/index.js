3e3bae53cb675af26f25ff093a50fde5a880878d
JULIAN DARIO GONZALEZ TOLEDO <julian.gonzalez@hokma.ai>
2024-07-17 20:52:29 -0500

    fix: se arregla animacion sidebar

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,13 @@ const close_icon = document.getElementById("aside__close_icon");
 const aside_menu = document.getElementById("aside_menu");
 
 list_icon.addEventListener("click", () => {
-    aside_menu.style.display = "block";
+    aside_menu.style.width = "500px";
+    aside_menu.style.paddingTop = "15px";
+    aside_menu.style.paddingLeft = "100px";
 });
 
 close_icon.addEventListener("click", () => {
-    aside_menu.style.display = "none";
+    aside_menu.style.width = "0";
+    aside_menu.style.paddingTop = "0";
+    aside_menu.style.paddingLeft = "0";
 });
